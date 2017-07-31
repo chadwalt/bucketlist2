92216781871c79b7653f4db411d6bafc3b18b257
chad <chadwalt@outlook.com>
2017-07-31 06:20:01 +0300

    Adding configurations to the config and run files

---
+++ config.py
@@ -0,0 +1,21 @@
+
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "hs@kdkDke3%@dkl-/"
+
+# Secret key for signing cookies
+SECRET_KEY = "@ksd-??2dd()1`f3323"
