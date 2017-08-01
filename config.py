d03880e2b5392cc60e50232ff572c9199c8439cc
chadwalt <chadwalt@outlook.com>
2017-08-01 06:52:19 +0300

    Updating the config file

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+""" Define configurations for the application. """
+
+# Define the application directory
+import os
 
 # Statement for enabling the development environment
 DEBUG = True
 
-# Define the application directory
-import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database - we are working with
-# SQLite for this example
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
