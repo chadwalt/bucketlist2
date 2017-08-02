a4974ba753ff7a4085f355c39131529daa6f5343
chadwalt <chadwalt@outlook.com>
2017-08-02 19:29:06 +0300

    Adding a docstring and also importing the create app function

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+""" This will be used to run the application."""
+
+## Import the os module
+import os
+
+## Import the app => create_app function.
+from app import create_app
+
 from app import app
 
 if __name__ == '__main__':
