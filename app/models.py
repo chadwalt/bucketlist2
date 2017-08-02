a9666a5f6caecf8e5b4950270eb05cc4814527bd
chadwalt <chadwalt@outlook.com>
2017-08-02 21:06:30 +0300

    Adding columns definitions to the users table.

--- app/models.py
+++ app/models.py
@@ -1,2 +1,18 @@
 """ This is going to contain the models for the application. Creating users, buckets and bucketitems"""
 
+## Import the database.
+from app import db
+
+## This is for creating the users.
+class Users(db.Model):
+    """ This class represents the users table. """
+
+    __tablename__ = 'users' ## Defining the table name.
+
+    ## Creating the columns for the table.
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(20))
+    sur_name = db.Column(db.String(20))
+    usename = db.Column(db.String(20))
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(30))
