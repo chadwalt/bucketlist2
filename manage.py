5d96c72e0b7d9be8e05bb313fc1061f8a9a534d1
chad <chadwalt@outlook.com>
2017-08-02 22:48:03 +0300

    Adding necessary imports to the file

--- manage.py
+++ manage.py
@@ -1,1 +1,7 @@
 """ This will manage the migrations of the database. """
+
+import os
+from flask_script import Manager ## manages a set of commands.
+from flask_migrate import Migrate, MigrateCommand
+from app import db, create_app # Import the database and the create app method.
+from app import models ## Import the models
