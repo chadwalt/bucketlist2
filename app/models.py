5455d99bbfddd2c4266c63cc6a8de075dc515b47
chad <chadwalt@outlook.com>
2017-08-03 05:29:25 +0300

    Adding an init method  to the buckets and adding init, save,delete, get, rep to the bucketitems class

--- app/models.py
+++ app/models.py
@@ -54,6 +54,11 @@ class Buckets(db.Model):
     name = db.Column(db.String(100))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
 
+    ## Initializing it.
+    def __init__(self, name, date_created):
+        self.name = name
+        self.date_created = date_created
+
     ## Save the bucket.
     def save(self):
         db.session.add(self)
@@ -74,5 +79,39 @@ class Buckets(db.Model):
 
 """ This is for the bucketitems """
 class Bucketitems():
-    
+    """ This represents the buckets table """
+
+    __tablename__ = "bucketitems"
+
+    ## Creating the columns for the table.
+    id = db.Column(db.Integer, primary_key = True, sequence(increment=1))
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(300))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    bucket_id = db.Column(db.Integer,db.ForeignKey('buckets.id'))
+
+    ## Initializing it.
+    def __init__(self, name, description, date_created, bucket_id):
+        self.name = name
+        self.description = description
+        self.date_created = date_created
+        self.bucket_id = bucket_id        
+
+    ## Save the bucket.
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    ## Get all the buckets.
+    def get_all():
+        return Bucketsitems.query.all()
+
+    ## Delete the bucket.
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    ## Create an object representation.
+    def __rep__(self):
+        return "<Bucketitems: {}>".format(self.name)
 
