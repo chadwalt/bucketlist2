669c6e3910b0812e134648600ffeab47566302ef
chad <chadwalt@outlook.com>
2017-08-03 05:53:30 +0300

    Adding the migrate functionaliy

--- manage.py
+++ manage.py
@@ -5,3 +5,12 @@ from flask_script import Manager ## manages a set of commands.
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app # Import the database and the create app method.
 from app import models ## Import the models
+
+app = create_app(config_name = os.getenv('APP_SETTINGS'))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add.command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
