1b79f32f4fbf4094ef6e49f83550da1d5012603f
chad <chadwalt@outlook.com>
2017-08-04 10:20:17 +0300

    Adding test for the encode token

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,3 +9,24 @@ sys.path.append(topdir)
 
 ## Import the app from the app folder.
 from app import create_app, db
+
+## Import the models.
+from app.models import Users
+
+class TestUserModel(unittest.TestCase):
+        
+    def test_encode_auth_token(self):
+        user = Users(
+            first_name = 'Timothy',
+            sur_name = 'Kyadondo',
+            username = 'chadwalt',
+            password='test',
+            email='test@test.com',            
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+
+if __name__ == '__main__':
+    unittest.main()
