884338a9ee0220dbfdab77fb2b64cca8ab0cd508
chad <chadwalt@outlook.com>
2017-08-04 12:39:03 +0300

    Added model for the BlacklistToken

--- app/models.py
+++ app/models.py
@@ -153,3 +153,19 @@ class Bucketitems(db.Model):
     def __rep__(self):
         return "<Bucketitems: {}>".format(self.name)
 
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
