bdafe025d19e0b3b65eb6a47cd5a4f2ed5ee2c6e
chadwalt <chadwalt@outlook.com>
2017-08-27 00:28:37 +0300

    Created bucket_items file for creating, editing, viewing and deleting bucket items from the application

---
+++ app/bucketlist_items.py
@@ -0,0 +1,275 @@
+from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+import re
+from instance.config import app_config, ITEMS_PER_PAGE
+from flask import request, jsonify, abort,render_template
+from flask_bcrypt import Bcrypt
+from flasgger import Swagger
+from flask_cors import CORS, cross_origin
+
+# initialize sql-alchemy
+db = SQLAlchemy()
+
+class Bucketlist_item:
+    ## Import the db configuration
+    from manage import *
+
+    CORS(app) ## Enable Cross Site Origin.
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
+    ## This will handle the routes if he route does not exist, it will return the 404 errors.
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return jsonify({'error': 'Page not found.'}), 404
+
+    ## This route is for creating a bucketlist item.
+    @app.route('/bucketlists/<int:id>/items/', methods=['POST'])
+    def add_bucketlists_items(id):
+        """ Add Buckets Items.
+        Please provide all the required fields.
+        ---
+        tags:
+         - Bucketlist Items
+        consumes:
+         - "application/x-www-form-urlencoded"
+        produces:
+         - "application/json"
+        parameters:
+         -  name: Authorization
+            in: header
+            type: string
+            description: Auth Token
+            required: true
+         -  name: id
+            in: path
+            type: integer
+            description: E.g ID of the bucket
+            required: true
+         -  name: name
+            in: formData
+            type: string
+            description: Name of the bucket item
+            required: true
+         -  name: description
+            in: formData
+            type: string
+            description: Detail about the item
+            required: true
+        responses:
+            200:
+                description: Bucket Item added successfully.
+        """
+        if request.method == 'POST': ## Add bucket items if the request is a POST.
+            ## Get the authentication token from the header.
+            token = request.headers['Authorization']
+
+            if token:
+                ## Decode the token to get the user_id
+                user_id = Users.decode_auth_token(token)
+                if isinstance(user_id, str):
+                    return jsonify({'success': False, 'msg': 'Invalid authentication token. Please login again.'})
+
+            name = request.form['name']
+            description = request.form['description']
+            bucket_id = id
+
+            if name and bucket_id:
+                bucketitem = Bucketitems(name, description, bucket_id)
+                bucketitem.save()
+
+                result = {
+                    'id': bucketitem.id,
+                    'name': bucketitem.name,
+                    'description': bucketitem.description,
+                    'date_created': bucketitem.date_created,
+                    'bucket_id': bucketitem.bucket_id
+                }
+
+                return jsonify(result)
+
+    ## This route is for getting bucketlist items.
+    @app.route('/bucketlists/<int:id>/items/', methods=['GET'])
+    def get_bucketlists_items(id):
+        """ Get all Buckets Items.
+        Please provide all the required fields.
+        ---
+        tags:
+         - Bucketlist Items
+        consumes:
+         - "application/x-www-form-urlencoded"
+        produces:
+         - "application/json"
+        parameters:
+         -  name: Authorization
+            in: header
+            type: string
+            description: Auth Token
+            required: true
+         -  name: id
+            in: path
+            type: integer
+            description: ID of the Bucketlist
+            required: true
+         -  name: page
+            in: query
+            type: integer
+            description: The page to view
+            required: false
+         -  name: rows
+            in: query
+            type: integer
+            description: Number of records/rows to return.
+            required: false
+        responses:
+            200:
+                description: All Bucket items.
+        """
+        if request.method == 'GET': ## Get bueckt items if the request if a GET
+            ## Get the authentication token from the header.
+            token = request.headers['Authorization']
+
+            if token:
+                ## Decode the token to get the user_id
+                user_id = Users.decode_auth_token(token)
+                if isinstance(user_id, str):
+                    return jsonify({'success': False, 'msg': 'Invalid authentication token. Please login again.'})
+
+            if not request.args.get('page'):
+                page = 1
+            else:
+                page = int(request.args.get('page'))
+
+            if not request.args.get('rows'):
+                rows = ITEMS_PER_PAGE
+            else:
+                rows = int(request.args.get('rows'))
+
+            bucketitems = Bucketitems.query.filter_by(bucket_id=id).paginate(page, rows, False).items
+
+            if not bucketitems:
+                abort(404) ## Raise not found error.
+
+            results = []
+
+            for bucketitem in bucketitems:
+                obj = {
+                    'id': bucketitem.id,
+                    'name': bucketitem.name,
+                    'description': bucketitem.description,
+                    'date_created': bucketitem.date_created,
+                    'bucket_id': bucketitem.bucket_id
+                }
+                results.append(obj)
+
+            return jsonify(results);
+
+    ## This route is for Updating a bucket.
+    @app.route('/bucketlists/<int:id>/items/<int:item_id>', methods=['PUT'])
+    def update_bucketitems_id(id, item_id = None):
+        """ Edit Bucket.
+        Please provide all the required fields.
+        ---
+        tags:
+         - Bucketlist Items
+        consumes:
+         - "application/x-www-form-urlencoded"
+        produces:
+         - "application/json"
+        parameters:
+         -  name: Authorization
+            in: header
+            type: string
+            description: Auth Token
+            required: true
+         -  name: id
+            in: path
+            type: integer
+            required: true
+            description: The ID of the bucket
+         -  name: item_id
+            in: path
+            type: integer
+            required: true
+            description: The ID of the bucketlist item
+         -  name: name
+            in: formData
+            type: string
+            required: true
+            description: The name of the bucketlist item
+         -  name: description
+            in: formData
+            type: string
+            required: true
+            description: A breif description of the bucketlist item.
+        """
+        if request.method == 'PUT': ## Save bucket if the request is a PUT.
+            ## Get the authentication token from the header.
+            token = request.headers['Authorization']
+
+            if token:
+                ## Decode the token to get the user_id
+                user_id = Users.decode_auth_token(token)
+                if isinstance(user_id, str):
+                    return jsonify({'success': False, 'msg': 'Invalid authentication token. Please login again.'})
+
+            bucketitem = Bucketitems.query.get(item_id)
+            name = str(request.form['name'])
+            description = str(request.form['description'])
+
+            bucketitem.name  = name
+            bucketitem.description = description
+            bucketitem.save()
+
+            results = {
+                'id': bucketitem.id,
+                'name': bucketitem.name,
+                'description': bucketitem.description,
+                'date_created': bucketitem.date_created,
+                'success': True,
+                'msg': 'Bucketitem updated successfully'
+                }
+
+            return jsonify(results)
+
+    @app.route('/bucketlists/<int:id>/items/<int:item_id>', methods=['DELETE'])
+    def bucketitems_id(id, item_id = None):
+        """ Delete Bucket item.
+        Please provide all the required fields.
+        ---
+        tags:
+         - Bucketlist Items
+        consumes:
+         - "application/x-www-form-urlencoded"
+        produces:
+         - "application/json"
+        parameters:
+         -  name: Authorization
+            in: header
+            type: string
+            description: Auth Token
+            required: true
+         -  name: id
+            in: path
+            type: integer
+            required: true
+         -  name: item_id
+            in: path
+            type: integer
+            required: true
+        """
+        if request.method == 'DELETE': ## Delete bucket if the request is a DELETE.
+            ## Get the authentication token from the header.
+            token = request.headers['Authorization']
+
+            if token:
+                ## Decode the token to get the user_id
+                user_id = Users.decode_auth_token(token)
+                if isinstance(user_id, str):
+                    return jsonify({'success': False, 'msg': 'Invalid authentication token. Please login again.'})
+
+            bucketitem = Bucketitems.query.get(item_id)
+            if not bucketitem:
+                return jsonify({'success': False, 'msg': 'Bucketlist item with id {} does not exist'.format(item_id)})
+            bucketitem.delete()
+
+            return jsonify({'success': True, 'msg': 'Bucketlist {} deleted successfully'.format(bucketitem.id)})
