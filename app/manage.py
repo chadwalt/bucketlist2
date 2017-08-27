b2bfc38337f5d25e4bb49de948cf419dad6dbe63
chadwalt <chadwalt@outlook.com>
2017-08-27 11:52:06 +0300

    Added cross site origin

--- app/manage.py
+++ app/manage.py
@@ -6,6 +6,7 @@ from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config, ITEMS_PER_PAGE
 from flasgger import Swagger
+from flask_cors import CORS, cross_origin
 
 # initialize sql-alchemy
 db = SQLAlchemy()
@@ -19,4 +20,6 @@ swagger = Swagger(app) ## Adding swagger.
 app.config.from_object(app_config[config_name])
 app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app) ## Enable Cross Site Origin.
+app.config['CORS_HEADERS'] = 'Content-Type'
 db.init_app(app)
