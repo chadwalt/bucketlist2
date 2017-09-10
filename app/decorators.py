33d2ba6f81aa8e7670a945c65388568170e90338
chadwalt <chadwalt04@gmail.com>
2017-09-10 12:04:23 +0300

    ch-Add decorators

---
+++ app/decorators.py
@@ -0,0 +1,23 @@
+"""
+This is for the decorator functions.
+"""
+
+from functools import wraps
+from flask import request, jsonify
+from manage import Users
+
+
+def auth_token_required(f): ## The f represents the function called before the decorator.
+    @wraps(f)
+    def token_required(*args, **kwargs):
+        ## Get the authentication token from the header.
+        token = request.headers['Authorization']
+
+        if token:
+            ## Decode the token to get the user_id
+            user_id = Users.decode_auth_token(token)
+            if isinstance(user_id, str):
+                return jsonify({'success': False, 'msg': 'Invalid authentication token. Please login again.'})
+
+        return f(*args, **kwargs)
+    return token_required
