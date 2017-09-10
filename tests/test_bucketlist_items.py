916848c3d700b6c3a69b0e690e5e2c5ccd857499
chadwalt <chadwalt04@gmail.com>
2017-09-10 20:43:27 +0300

    Add status codes and fix import issues

--- tests/test_bucketlist_items.py
+++ tests/test_bucketlist_items.py
@@ -8,15 +8,14 @@ topdir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(topdir)
 
 ## Import the app from the app folder.
-from app import create_app, db
+from app.manage import app, db
 
-""" This class will test the users """
 class ApiTestCase(unittest.TestCase):
     ## Set it up.
     def setUp(self):
         ## Defining test variables and initialize the appliction.
 
-        self.app = create_app(config_name='testing');
+        self.app = app
         self.client = self.app.test_client
         self.user = {'first_name': 'Timothy', 'sur_name' : 'Kyadondo', 'username': 'chadwalt', 'password': '123', 'email': 'chadwalt@outlook.com'}
         self.bucketitems = {'name': 'Climbing More', 'description': 'Touching the clouds', 'bucket_id': '1'}
@@ -39,7 +38,7 @@ class ApiTestCase(unittest.TestCase):
 
         resp = self.client().post('/bucketlists/', data = self.bucket, headers=dict(Authorization=token))
         resp = self.client().post('/bucketlists/1/items/', data = self.bucketitems, headers=dict(Authorization=token))
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 201)
         self.assertIn('Climbing', str(resp.data)) ## Searches for climbing in the users string.
 
     def test_get_all_bucketitems(self):
@@ -52,7 +51,7 @@ class ApiTestCase(unittest.TestCase):
 
         resp = self.client().post('/bucketlists/', data = self.bucket, headers=dict(Authorization=token))
         resp = self.client().post('/bucketlists/1/items/', data = self.bucketitems, headers=dict(Authorization=token))
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 201)
 
         resp = self.client().get('/bucketlists/1/items/', headers=dict(Authorization=token))
         self.assertEqual(resp.status_code, 200) ## Test if the response is successfully loaded.
@@ -71,7 +70,7 @@ class ApiTestCase(unittest.TestCase):
 
         form_data = {'name': 'walking on the moon', 'description': 'Go by the space craft'}
         resp = self.client().put('/bucketlists/1/items/1', data = form_data, headers=dict(Authorization=token))
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 201)
 
         self.assertIn('true', str(resp.data))
 
@@ -83,8 +82,11 @@ class ApiTestCase(unittest.TestCase):
         resp_login = self.client().post('/auth/login', data = self.form_data) ## Login the user.
         token = json.loads(resp_login.data.decode())['auth_token'] ## Get the authentication token.
 
-        resp = self.client().delete('/bucketlists/1/items/1', headers=dict(Authorization=token))
-        self.assertEqual(resp.status_code, 200)
+        res = self.client().post('/bucketlists/', data = self.bucket, headers=dict(Authorization=token))
+        resp = self.client().post('/bucketlists/1/items/', data = self.bucketitems, headers=dict(Authorization=token)) ## Create the item.
+        result = self.client().delete('/bucketlists/1/items/1', headers=dict(Authorization=token))
+
+        self.assertEqual(resp.status_code, 201)
 
     def tearDown(self):
         """teardown all initialized variables."""
